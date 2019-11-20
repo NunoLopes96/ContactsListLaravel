6f9c79dcbeca4969343e552ef6a569b443f8c80d
Nuno Lopes <nuno@redweb.dk>
2019-11-20 13:13:12 +0000

    Improved index contacts view

--- views/contacts/index.php
+++ views/contacts/index.php
@@ -1,4 +1,8 @@
 <?=
-/** @var array $contacts */
+/** @var array $contacts - All contacts of the authenticated user. */
 
-\json_encode($contacts);
+\json_encode([
+    'data' => [
+        'contacts' => $contacts
+    ]
+]);
